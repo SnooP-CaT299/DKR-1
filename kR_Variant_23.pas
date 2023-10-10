38e46ac4479a4475c710602184a9bfce2a10906b
SnooP-CaT299 <131278218+SnooP-CaT299@users.noreply.github.com>
2023-10-10 11:57:38 +0300

    Add files via upload

---
+++ kR_Variant_23.pas
@@ -0,0 +1,21 @@
+﻿program DKR;
+
+var x,f:real;
+begin
+  x:=-10;
+  while x<=6 do
+  begin
+   if x<-8 then
+    f:=(cos(2*x)/86)+ln(x)
+   else if (x>=-8) and (x<4)then
+    f:=(sin(x)+exp(x))
+   else if (x>=-3) and (x<4) then
+    f:= x*x*x - cos(2*x)
+   else if x>=4 then
+     f:= x*x+cos(x*2);
+   
+  
+    writeln(' Для x= ' + x + ' Значение функции = ' + f);
+    x:=x+0.1;
+  end;
+end.
